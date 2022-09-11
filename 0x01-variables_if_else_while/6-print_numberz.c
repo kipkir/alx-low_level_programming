e8ce0f8ee8463eac60da853ae0e5f66ef9169821
kipkir <kipkiruitessot@gmail.com>
2022-09-11 19:45:18 +0000

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  *  main - Entry point
  *
@@ -10,7 +12,7 @@ int main(void)
 
 	for (s = '0'; s <= '9'; s++)
 		putchar(s);
-		putchar('\n');
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
