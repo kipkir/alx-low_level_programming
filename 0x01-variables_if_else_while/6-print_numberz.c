f689cd9da7af1db40a6c74e0ed0afc607a58ba9b
kipkir <kipkiruitessot@gmail.com>
2022-09-11 19:38:18 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+	int s;
+
+	for (s = '0'; s <= '9'; s++)
+		putchar(s);
+		putchar('\n');
+
+		return (0);
+}
