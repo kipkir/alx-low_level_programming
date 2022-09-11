07864e2e22cc27ee18c132a69e8fc36714df3fe7
kipkir <kipkiruitessot@gmail.com>
2022-09-11 19:00:31 +0000

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ *  main - Entry point
  *
- * Return: Always 0
+ *   Return: Always 0
  */
 int main(void)
 {
@@ -13,10 +13,10 @@ int main(void)
 	e = 'e';
 	q = 'q';
 
-	for (low = 'a' ; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low ++)
 	{
 		if (low != e && low != q)
-			putchar(low)
+			putchar(low);
 	}
 	printf("\n");
 
