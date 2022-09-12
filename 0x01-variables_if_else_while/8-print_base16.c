939147d35a6013cfad3b965977f43ebaa74d3217
kipkir <kipkiruitessot@gmail.com>
2022-09-12 07:21:40 +0000

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include < stdio.h>
 #include <stdlib.h>
-#include <time>
+#include <time.h>
 /**
  *  main -Entry point
  *
@@ -16,6 +16,6 @@ int main(void)
 	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
 	putchar('\n');
-
-	return (0);
+	
+	return(0);
 }
